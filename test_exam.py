c48d52795f89da6f320a8006119b51da915723ad
Diego Velásquez <ciegovr@gmail.com>
2024-08-05 11:44:46 -0500

    Add docstrings and solve unit tests

--- test_exam.py
+++ test_exam.py
@@ -1,5 +1,6 @@
 import pytest
-from inventario import Inventario, Producto
+
+from exam import Inventario, Producto
 
 
 def test_crear_producto():
@@ -9,6 +10,7 @@ def test_crear_producto():
     assert producto.precio == 200.00
     assert producto.stock == 50
 
+
 def test_agregar_producto_a_inventario():
     inventario = Inventario()
     producto = Producto(1, 'Pantalones', 200.00, 50)
@@ -16,9 +18,24 @@ def test_agregar_producto_a_inventario():
     assert len(inventario.productos) == 1
     assert inventario.productos[1] == producto
 
+
 def test_eliminar_producto_de_inventario():
     inventario = Inventario()
     producto = Producto(1, 'Pantalones', 200.00, 50)
     inventario.agregar_producto(producto)
     inventario.eliminar_producto(1)
     assert len(inventario.productos) == 0
+
+
+def test_actualizar_producto_de_inventario():
+    producto = Producto(1, 'Pantalones', 200.00, 50)
+    inventario = Inventario()
+    inventario.agregar_producto(producto)
+    codigo = 1
+    nuevo_precio = 500.00
+    stock = 10
+    inventario.actualizar_producto(codigo, nuevo_precio, stock)
+    producto_actualizado = inventario.productos[1]
+    assert producto_actualizado.codigo == codigo
+    assert producto_actualizado.stock == stock
+    assert producto_actualizado.precio == nuevo_precio
